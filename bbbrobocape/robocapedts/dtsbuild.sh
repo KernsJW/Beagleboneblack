e3088652a4e9d124af1aeda6faa928f3f683a419
KernsJW <Kernsjw@gmail.com>
2014-09-04 22:48:19 -0400

    Update dtsbuild.sh

--- bbbrobocape/robocapedts/dtsbuild.sh
+++ bbbrobocape/robocapedts/dtsbuild.sh
@@ -2,8 +2,10 @@
 echo compiling dts files
 dtc -O dtb -o BB-SPI1-01-00A0.dtbo -b 0 -@ BB-SPI1-01-00A0.dts
 cp BB-SPI1-01-00A0.dtbo /lib/firmware/
-dtc -O dtb -o DM-GPIO-Test-00A0.dtbo -b 0 -@ DM-GPIO-Test.dts
-cp DM-GPIO-Test-00A0.dtbo /lib/firmware
+dtc -O dtb -o BB-SPI0-01-00A0.dtbo -b 0 -@ BB-SPI0-01-00A0.dts
+cp BB-SPI0-01-00A0.dtbo /lib/firmware
+dtc -O dtb -o RoboCapeGpio-00A0.dtbo -b 0 -@ RoboCapeGpio.dts
+cp RoboCapeGpio-00A0.dtbo /lib/firmware
 dtc -O dtb -o pwm_test_P8_13-00A0.dtbo -b 0 -@ pwm_test_P8_13-00A0.dts
 cp pwm_test_P8_13-00A0.dtbo /lib/firmware
 dtc -O dtb -o pwm_test_P8_19-00A0.dtbo -b 0 -@ pwm_test_P8_19-00A0.dts
